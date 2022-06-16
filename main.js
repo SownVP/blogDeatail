1001b9d8faa9895596065445a1881398eb558c95
SownVP <trinhvanson0807@gmail.com>
2022-06-16 13:24:53 +0700

    responsive

--- main.js
+++ main.js
@@ -8,3 +8,16 @@ document.querySelector('.comment_Icon__open').addEventListener('click', function
 document.querySelector('.comment_Icon__close').addEventListener('click', function(){
     document.querySelector('.comment_Wrapper').style.display = 'none'
 })
+
+document.querySelectorAll('.post_Reaction').forEach(function(post_Reaction, index){
+    post_Reaction.addEventListener('click', function(){
+        document.querySelectorAll('.post_Reaction__active')[index].style.display = 'inline';
+        post_Reaction.style.display = 'none'
+    })
+})
+document.querySelectorAll('.post_Reaction__active').forEach(function(post_Reaction__active, index){
+    post_Reaction__active.addEventListener('click', function(){
+        document.querySelectorAll('.post_Reaction')[index].style.display = 'inline';
+        post_Reaction__active.style.display = 'none'
+    })
+})
